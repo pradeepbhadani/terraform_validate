5d660cf6187353065766fa33bb0aaa0245ab016c
Pradeep Bhadani <pradeep.bhadani@gmail.com>
2017-05-31 16:35:22 +0000

    Fix test.

--- terraform_validate/fixtures/boolean_compare/1.tf
+++ terraform_validate/fixtures/boolean_compare/1.tf
@@ -1,6 +1,7 @@
 resource "aws_db_instance" "rds_db_instance" {
   storage_encrypted1 = true
-#  storage_encrypted3 = "True"
-#  storage_encrypted4 = "true"
-#  storage_encrypted5 = "trUE"
+  storage_encrypted2 = true
+  storage_encrypted3 = "True"
+  storage_encrypted4 = "true"
+  storage_encrypted5 = "trUE"
 }
